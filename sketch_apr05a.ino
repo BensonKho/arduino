9802898aa5a496936efc409ec32e3a0bc71a1499
S-T-R-i-F-E <46480330+S-T-R-i-F-E@users.noreply.github.com>
2019-08-30 21:50:47 +0530

    Update sketch_apr05a.ino

--- sketch_apr05a.ino
+++ sketch_apr05a.ino
@@ -1,9 +1,9 @@
 //gesture control using arduino uno
 
-const int trigger1 = 2; //Trigger pin of 1st Sesnor
-const int echo1 = 3; //Echo pin of 1st Sesnor
-const int trigger2 = 4; //Trigger pin of 2nd Sesnor
-const int echo2 = 5;//Echo pin of 2nd Sesnor
+const int trigger1 = 2; //Trigger pin of 1st Sensor
+const int echo1 = 3; //Echo pin of 1st Sensor
+const int trigger2 = 4; //Trigger pin of 2nd Sensor
+const int echo2 = 5;//Echo pin of 2nd Sensor
 
 long time_taken;
 int dist,distL,distR;
